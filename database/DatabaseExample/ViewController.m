f60dab522a3b6a85547ff9c9e3a14df532487e3e
Mike McDonald <mpmcdonald@google.com>
2015-11-06 12:41:03 -0800

    Adding .clang-format
    Change-Id: Ib4119c98644c165e6aac029d1dd6ed49997fd8d8

--- database/DatabaseExample/ViewController.m
+++ database/DatabaseExample/ViewController.m
@@ -21,17 +21,17 @@
   UInt32 _userInt;
 }
 
-
-#pragma mark -
-#pragma mark UIViewController lifecycle methods
+#pragma mark - UIViewController lifecycle methods
 - (void)viewDidLoad {
   [super viewDidLoad];
 
   // This will change pending in cl/106974108
-  NSDictionary *plist = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"GoogleService-Info" ofType:@"plist"]];
+  NSDictionary *plist = [NSDictionary
+      dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"GoogleService-Info"
+                                                                   ofType:@"plist"]];
   if (plist) {
     self.ref = [[Firebase alloc] initWithUrl:plist[@"FIREBASE_DATABASE_URL"]];
-    [self.ref unauth]; //Remove this hack pending b/25426664
+    [self.ref unauth];  // Remove this hack pending b/25426664
   }
 
   _userInt = arc4random();
@@ -42,25 +42,30 @@
 - (void)viewWillAppear:(BOOL)animated {
   [self.messages removeAllObjects];
   // Listen for new messages in the Firebase database
-  _refHandle = [[self.ref childByAppendingPath:@"messages"] observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
-    [self.messages addObject:snapshot];
-    [self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:[self.messages count]-1 inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
-  }];
+  _refHandle = [[self.ref childByAppendingPath:@"messages"]
+      observeEventType:FEventTypeChildAdded
+             withBlock:^(FDataSnapshot *snapshot) {
+               [self.messages addObject:snapshot];
+               [self.tableView insertRowsAtIndexPaths:@[
+                 [NSIndexPath indexPathForRow:[self.messages count] - 1 inSection:0]
+               ] withRowAnimation:UITableViewRowAnimationAutomatic];
+             }];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
   [self.ref removeObserverWithHandle:_refHandle];
 }
 
-#pragma mark -
-#pragma mark UITableViewDataSource protocol methods
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#pragma mark - UITableViewDataSource protocol methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
   return [self.messages count];
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
   // Dequeue cell
-  UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"tableViewCell" forIndexPath:indexPath];
+  UITableViewCell *cell =
+      [self.tableView dequeueReusableCellWithIdentifier:@"tableViewCell" forIndexPath:indexPath];
 
   // Unpack message from Firebase DataSnapshot
   FDataSnapshot *snapshot = self.messages[indexPath.row];
@@ -71,11 +76,13 @@
   return cell;
 }
 
-#pragma mark -
-#pragma mark UITableViewDataSource protocol methods
--(BOOL)textFieldShouldReturn:(UITextField *)textField {
+#pragma mark - UITableViewDataSource protocol methods
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
   // Push data to Firebase Database
-  [[[self.ref childByAppendingPath:@"messages"] childByAutoId] setValue:@{@"name": [NSString stringWithFormat:@"User %d", _userInt], @"text":textField.text}];
+  [[[self.ref childByAppendingPath:@"messages"] childByAutoId] setValue:@{
+    @"name" : [NSString stringWithFormat:@"User %d", _userInt],
+    @"text" : textField.text
+  }];
   textField.text = @"";
   return YES;
 }

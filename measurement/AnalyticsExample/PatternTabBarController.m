c75389d059a93ef68b4ae1adc1d91b6f2c26fbbd
Ian Barber <ianbarber@google.com>
2015-11-12 22:27:54 -0800

    Initial port of GA to measurement
    Change-Id: I19323e0e978bf42c4bb045fa9177644629b085f4

---
+++ measurement/AnalyticsExample/PatternTabBarController.m
@@ -0,0 +1,43 @@
+//
+//  Copyright (c) 2015 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "PatternTabBarController.h"
+
+@import Firebase.Core;
+
+@implementation PatternTabBarController
+
+- (void)didTapShare:(id)sender {
+  // [START custom_event_objc]
+  [GMRAppMeasurement logEventWithName:kGMREventShare parameters:@{
+                                                                  kGMRParameterContentType:@"cont",
+                                                                  kGMRParameterContentID:@"1"
+                                                                  }];
+  // [END custom_event_objc]
+
+  NSString *title = [NSString stringWithFormat:@"Share: %@",
+                     self.selectedViewController.title];
+  NSString *message =
+      @"Share event sent to Analytics; actual share not implemented in this quickstart";
+  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                  message:message
+                                                 delegate:nil
+                                        cancelButtonTitle:@"Ok"
+                                        otherButtonTitles:nil];
+  [alert show];
+}
+
+@end

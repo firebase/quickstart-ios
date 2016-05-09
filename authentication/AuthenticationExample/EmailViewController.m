c7737d7cf6f99388cf7f047539529052fe7601c1
Ian Barber <ianbarber@google.com>
2016-05-09 15:20:09 -0700

    Auth tweaks - Minor UI updates - Change picker in Objc to an alert controller. We are iOS 8+ due to the dependencies on Facebook anyway and I think this is a cleaner implementation. : Change-Id: I562fb6169689f1d17279f2b2e3c193f1fe19e4fe

--- authentication/AuthenticationExample/EmailViewController.m
+++ authentication/AuthenticationExample/EmailViewController.m
@@ -20,8 +20,8 @@
 @import FirebaseAuth;
 
 @interface EmailViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *emailField;
-@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property(weak, nonatomic) IBOutlet UITextField *emailField;
+@property(weak, nonatomic) IBOutlet UITextField *passwordField;
 @end
 
 @implementation EmailViewController
@@ -54,30 +54,34 @@
  @brief Requests a "password reset" email be sent.
  */
 - (IBAction)didRequestPasswordReset:(id)sender {
-  [self showTextInputPromptWithMessage:@"Email:"
-                       completionBlock:^(BOOL userPressedOK, NSString *_Nullable userInput) {
-                         if (!userPressedOK || !userInput.length) {
-                           return;
-                         }
+  [self
+      showTextInputPromptWithMessage:@"Email:"
+                     completionBlock:^(BOOL userPressedOK, NSString *_Nullable userInput) {
+                       if (!userPressedOK || !userInput.length) {
+                         return;
+                       }
 
-                         [self showSpinner:^{
-                           // [START password_reset]
-                           [[FIRAuth auth] sendPasswordResetWithEmail:userInput
-                                                           completion:^(NSError * _Nullable error) {
-                                                             // [START_EXCLUDE]
-                                                             [self hideSpinner:^{
-                                                               if (error) {
-                                                                 [self showMessagePrompt:error.localizedDescription];
-                                                                 return;
-                                                               }
+                       [self showSpinner:^{
+                         // [START password_reset]
+                         [[FIRAuth auth]
+                             sendPasswordResetWithEmail:userInput
+                                             completion:^(NSError *_Nullable error) {
+                                               // [START_EXCLUDE]
+                                               [self hideSpinner:^{
+                                                 if (error) {
+                                                   [self
+                                                       showMessagePrompt:error
+                                                                             .localizedDescription];
+                                                   return;
+                                                 }
 
-                                                               [self showMessagePrompt:@"Sent"];
-                                                             }];
-                                                             // [END_EXCLUDE]
-                                                           }];
-                           // [END password_reset]
-                         }];
+                                                 [self showMessagePrompt:@"Sent"];
+                                               }];
+                                               // [END_EXCLUDE]
+                                             }];
+                         // [END password_reset]
                        }];
+                     }];
 }
 
 /** @fn getProvidersForEmail
@@ -85,64 +89,78 @@
  and displays the result.
  */
 - (IBAction)didGetProvidersForEmail:(id)sender {
-  [self showTextInputPromptWithMessage:@"Email:"
-                       completionBlock:^(BOOL userPressedOK, NSString *_Nullable userInput) {
-                         if (!userPressedOK || !userInput.length) {
-                           return;
-                         }
+  [self
+      showTextInputPromptWithMessage:@"Email:"
+                     completionBlock:^(BOOL userPressedOK, NSString *_Nullable userInput) {
+                       if (!userPressedOK || !userInput.length) {
+                         return;
+                       }
 
-                         [self showSpinner:^{
-                           // [START get_providers]
-                           [[FIRAuth auth] fetchProvidersForEmail:userInput
-                                                       completion:^(NSArray<NSString *> *_Nullable providers, NSError *_Nullable error) {
-                                                         // [START_EXCLUDE]
-                                                         [self hideSpinner:^{
-                                                           if (error) {
-                                                             [self showMessagePrompt:error.localizedDescription];
-                                                             return;
-                                                           }
+                       [self showSpinner:^{
+                         // [START get_providers]
+                         [[FIRAuth auth]
+                             fetchProvidersForEmail:userInput
+                                         completion:^(NSArray<NSString *> *_Nullable providers,
+                                                      NSError *_Nullable error) {
+                                           // [START_EXCLUDE]
+                                           [self hideSpinner:^{
+                                             if (error) {
+                                               [self showMessagePrompt:error.localizedDescription];
+                                               return;
+                                             }
 
-                                                           [self showMessagePrompt:[providers componentsJoinedByString:@", "]];
-                                                         }];
-                                                         // [END_EXCLUDE]
-                                                       }];
-                           // [END get_providers]
-                         }];
+                                             [self showMessagePrompt:
+                                                       [providers componentsJoinedByString:@", "]];
+                                           }];
+                                           // [END_EXCLUDE]
+                                         }];
+                         // [END get_providers]
                        }];
+                     }];
 }
 
 - (IBAction)didCreateAccount:(id)sender {
-  [self showTextInputPromptWithMessage:@"Email:"
-                       completionBlock:^(BOOL userPressedOK, NSString *_Nullable email) {
-                         if (!userPressedOK || !email.length) {
-                           return;
-                         }
+  [self
+      showTextInputPromptWithMessage:@"Email:"
+                     completionBlock:^(BOOL userPressedOK, NSString *_Nullable email) {
+                       if (!userPressedOK || !email.length) {
+                         return;
+                       }
 
-                         [self showTextInputPromptWithMessage:@"Password:"
-                                              completionBlock:^(BOOL userPressedOK, NSString *_Nullable password) {
-                                                if (!userPressedOK || !password.length) {
-                                                  return;
-                                                }
+                       [self
+                           showTextInputPromptWithMessage:@"Password:"
+                                          completionBlock:^(BOOL userPressedOK,
+                                                            NSString *_Nullable password) {
+                                            if (!userPressedOK || !password.length) {
+                                              return;
+                                            }
 
-                                                [self showSpinner:^{
-                                                  // [START create_user]
-                                                  [[FIRAuth auth] createUserWithEmail:email password:password
-                                                                           completion:^(FIRUser * _Nullable user, NSError * _Nullable error) {
-                                                                             // [START_EXCLUDE]
-                                                                             [self hideSpinner:^{
-                                                                               if (error) {
-                                                                                 [self showMessagePrompt:error.localizedDescription];
-                                                                                 return;
-                                                                               }
-
-                                                                               [self showMessagePrompt:user.email];
-                                                                             }];
-                                                                             // [END_EXCLUDE]
-                                                                           }];
-                                                  // [END create_user]
-                                                }];
-                                              }];
-                       }];
+                                            [self showSpinner:^{
+                                              // [START create_user]
+                                              [[FIRAuth auth]
+                                                  createUserWithEmail:email
+                                                             password:password
+                                                           completion:^(FIRUser *_Nullable user,
+                                                                        NSError *_Nullable error) {
+                                                             // [START_EXCLUDE]
+                                                             [self hideSpinner:^{
+                                                               if (error) {
+                                                                 [self
+                                                                     showMessagePrompt:
+                                                                         error
+                                                                             .localizedDescription];
+                                                                 return;
+                                                               }
+                                                               NSString *msg = [NSString stringWithFormat:@"%@ created",
+                                                                                user.email];
+                                                               [self showMessagePrompt:msg];
+                                                             }];
+                                                             // [END_EXCLUDE]
+                                                           }];
+                                              // [END create_user]
+                                            }];
+                                          }];
+                     }];
 }
 
 @end

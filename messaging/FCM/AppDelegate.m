c59c5cbdcd80fccb130a8b611c55dce58f296b77
Arthur Thompson <arthurthompson@google.com>
2016-09-13 16:28:34 -0700

    Fix typo in Messaging AppDelegate

--- messaging/FCM/AppDelegate.m
+++ messaging/FCM/AppDelegate.m
@@ -62,7 +62,7 @@
   // Print message ID.
   NSLog(@"Message ID: %@", userInfo[@"gcm.message_id"]);
 
-  // Pring full message.
+  // Print full message.
   NSLog(@"%@", userInfo);
 }
 // [END receive_message]

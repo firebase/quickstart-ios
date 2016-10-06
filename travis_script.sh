4c21538ef40d23f6f7521cc23a1840757b26beaf
Ibrahim Ulukaya <ulukaya@gmail.com>
2016-10-06 14:23:16 -0400

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -7,12 +7,12 @@ set -e
 samples=( admob analytics app-indexing auth config crash database dynamiclinks invites messaging storage )
 
 # Work off travis
-if [[ -v TRAVIS_PULL_REQUEST ]]; then
-  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
-else
-  echo "TRAVIS_PULL_REQUEST: unset, setting to false"
-  TRAVIS_PULL_REQUEST=false
-fi
+#if [[ -v TRAVIS_PULL_REQUEST ]]; then
+#  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+#else
+#  echo "TRAVIS_PULL_REQUEST: unset, setting to false"
+#  TRAVIS_PULL_REQUEST=false
+#fi
 
 for sample in "${samples[@]}"
 do
@@ -21,4 +21,5 @@ do
   # Go to sample directory
   cd $sample
   pod install --repo-update
+  cd -
 done

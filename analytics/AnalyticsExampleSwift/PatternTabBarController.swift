f99c440590b2d3d41fa05317af07cc6986134745
Morgan Chen <morganchen12@gmail.com>
2017-10-30 17:56:10 -0700

    use spaces, not tabs
    Change-Id: I3bbcdfd7b8acac5dfaf7258691b590a0a6a37a12

--- analytics/AnalyticsExampleSwift/PatternTabBarController.swift
+++ analytics/AnalyticsExampleSwift/PatternTabBarController.swift
@@ -46,8 +46,8 @@ class PatternTabBarController: UITabBarController {
     let title = "Share: \(self.selectedViewController!.title!)",
         message = "Share event sent to Analytics; actual share not implemented in this quickstart",
 		alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-	alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-	present(alert, animated: true, completion: nil)
+    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+    present(alert, animated: true, completion: nil)
   }
 
   @IBAction func unwindToHome(_ segue: UIStoryboardSegue?) {

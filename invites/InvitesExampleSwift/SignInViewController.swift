e5e5826c31e68f81351e4cac27d17e2f9eb7ec1d
Valeriy Van <github@w7software.com>
2017-11-27 20:08:57 +0100

    Refactors if statement into guard. Breaks retain circle. (#398)
    * Refactors if statement into guard. Breaks retain circle.
    
    * Rolls back change of asyncAfter

--- invites/InvitesExampleSwift/SignInViewController.swift
+++ invites/InvitesExampleSwift/SignInViewController.swift
@@ -37,15 +37,15 @@ class SignInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDele
   }
 
   func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-    if let error = error {
+    guard error == nil else {
       // Something went wrong; for example, the user could haved clicked cancel.
       print("\(error.localizedDescription)")
-    } else {
-      // User Successfully signed in.
-      // TODO: Remove async after, when GIDSignIn is started getting called after dissmissVC
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
-        self.performSegue(withIdentifier: "SignedInScreen", sender: self)
-      }
+      return
+    }
+    // User Successfully signed in.
+    // TODO: Remove async after, when GIDSignIn is started getting called after dissmissVC
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
+      self.performSegue(withIdentifier: "SignedInScreen", sender: self)
     }
   }
 

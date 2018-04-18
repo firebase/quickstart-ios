752d885c62971c0dbde571d82abd756d177e50f7
Ibrahim Ulukaya <ulukaya@google.com>
2018-04-18 14:49:58 -0400

    add test targets to Functions sample

---
+++ functions/FunctionsExampleTests/FunctionsExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  FunctionsExampleTests.m
+//  FunctionsExampleTests
+//
+//  Created by Ibrahim Ulukaya on 4/18/18.
+//  Copyright © 2018 Google Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FunctionsExampleTests : XCTestCase
+
+@end
+
+@implementation FunctionsExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

72432eef7d8a12598d83eda085210e4206c6fbb1
Vladimir Samoylenko <vlad@the-red-queen.net>
2018-11-27 23:55:58 +0200

    Update AdMobExample to Swift 4.2

--- admob/AdMobExampleSwift/AppDelegate.swift
+++ admob/AdMobExampleSwift/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Use Firebase library to configure APIs
     FirebaseApp.configure()
     return true

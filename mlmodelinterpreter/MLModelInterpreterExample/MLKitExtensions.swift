111baa287ed6fa5e3fd19967c4516af0a893bc71
Valeriy Van <github@w7software.com>
2019-01-10 12:03:41 +0100

    Simplifies CGRect creation

--- mlmodelinterpreter/MLModelInterpreterExample/MLKitExtensions.swift
+++ mlmodelinterpreter/MLModelInterpreterExample/MLKitExtensions.swift
@@ -29,7 +29,7 @@ extension UIImage {
   /// - Returns: The scaled image or `nil` if image could not be resized.
   public func scaledImage(with size: CGSize) -> UIImage? {
     UIGraphicsBeginImageContextWithOptions(size, false, scale)
-    draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+    draw(in: CGRect(origin: .zero, size: size))
     let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
     UIGraphicsEndImageContext()
 

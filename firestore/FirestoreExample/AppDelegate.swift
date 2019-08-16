8cfd19becfce2609c4c95b4468325004cedac1c4
Morgan Chen <morganchen12@gmail.com>
2019-08-16 12:54:42 -0700

    Fix deprecation warning in Firestore quickstart

--- firestore/FirestoreExample/AppDelegate.swift
+++ firestore/FirestoreExample/AppDelegate.swift
@@ -25,11 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
-
-    let db = Firestore.firestore()
-    let settings = db.settings
-    settings.areTimestampsInSnapshotsEnabled = true
-    db.settings = settings
     return true
   }
 

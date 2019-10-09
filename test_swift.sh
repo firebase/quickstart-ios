af44eb35e59c5d64d7a342352e2a68edd4373115
Maksym Malyhin <mmaksym@google.com>
2019-10-09 14:03:03 -0400

    Test Swift examples as well.

---
+++ test_swift.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+EXIT_STATUS=0
+
+(xcodebuild \
+  -workspace ${SAMPLE}/${SAMPLE}Example.xcworkspace \
+  -scheme ${SAMPLE}ExampleSwift \
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 11' \
+  build \
+  test \
+  ONLY_ACTIVE_ARCH=YES \
+  | xcpretty) || EXIT_STATUS=$?
+
+  exit $EXIT_STATUS

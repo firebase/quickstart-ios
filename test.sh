cf7dc24a8297a5f635dfd246e3420e092b293083
Paul Beusterien <paulbeusterien@google.com>
2019-12-21 08:52:10 -0800

    Skip running tests if GoogleService-Info.plist's weren't decoded

--- test.sh
+++ test.sh
@@ -4,14 +4,25 @@ set -eo pipefail
 
 EXIT_STATUS=0
 
-(xcodebuild \
-  -workspace ${SAMPLE}/${SAMPLE}Example.xcworkspace \
-  -scheme ${SAMPLE}Example${SWIFT_SUFFIX} \
-  -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 11' \
-  build \
-  test \
-  ONLY_ACTIVE_ARCH=YES \
-  | xcpretty) || EXIT_STATUS=$?
+if [[ ! -z $encrypted_2858fa01aa14_key ]]; then
+    (xcodebuild \
+      -workspace ${SAMPLE}/${SAMPLE}Example.xcworkspace \
+      -scheme ${SAMPLE}Example${SWIFT_SUFFIX} \
+      -sdk iphonesimulator \
+      -destination 'platform=iOS Simulator,name=iPhone 11' \
+      build \
+      test \
+      ONLY_ACTIVE_ARCH=YES \
+      | xcpretty) || EXIT_STATUS=$?
+else
+    # Skip running tests if GoogleService-Info.plist's weren't decoded.
+    (xcodebuild \
+      -workspace ${SAMPLE}/${SAMPLE}Example.xcworkspace \
+      -scheme ${SAMPLE}Example${SWIFT_SUFFIX} \
+      -sdk iphonesimulator \
+      -destination 'platform=iOS Simulator,name=iPhone 11' \
+      build \
+      ONLY_ACTIVE_ARCH=YES \
+      | xcpretty) || EXIT_STATUS=$?
 
   exit $EXIT_STATUS

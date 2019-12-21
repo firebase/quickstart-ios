8e99e0800d419a5f2123ec4ea7464c025068c26a
Paul Beusterien <paulbeusterien@google.com>
2019-12-21 09:30:25 -0800

    Auth and RemoteConfig

--- config/ConfigExampleTests/AppTests.m
+++ config/ConfigExampleTests/AppTests.m
@@ -17,24 +17,20 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "FIRApp.h"
+#import "FIRRemoteConfig.h"
+
 @interface AppTests : XCTestCase
 @end
 
 @implementation AppTests
 
-- (void)setUp {
-  [super setUp];
-  // Put setup code here. This method is called before the invocation of each test method in the class.
-}
 
-- (void)tearDown {
-  // Put teardown code here. This method is called after the invocation of each test method in the class.
-  [super tearDown];
-}
-
-- (void)testExample {
-  // This is an example of a functional test case.
-  XCTAssert(YES, @"Pass");
+- (void)testRemoteConfig {
+  [FIRApp configure];
+  FIRRemoteConfig *remoteConfig = [FIRRemoteConfig remoteConfig];
+  XCTAssert([remoteConfig isKindOfClass:[FIRRemoteConfig class]]);
+  XCTAssertTrue([[remoteConfig configSettings] isDeveloperModeEnabled]);
 }
 
 @end
--- config/ConfigExampleUITests/ConfigExampleUITests.m
+++ config/ConfigExampleUITests/ConfigExampleUITests.m
@@ -7,34 +7,32 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "FIREGHelper.h"
 
-@interface ConfigExampleUITests : XCTestCase
-
+@interface ConfigUITest : XCTestCase
 @end
 
-@implementation ConfigExampleUITests
+@implementation ConfigUITest {
+  XCUIApplication *_app;
+}
 
 - (void)setUp {
-    [super setUp];
-    
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-    
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  [super setUp];
+  _app = [[XCUIApplication alloc] init];
+  [_app launch];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+- (void)testRemoteConfig {
+  // Verify that Remote Config Example app is launched successfully and its title is visible.
+  XCTAssertTrue([_app navigationBars][@"Remote Config Example"].exists);
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  // Tap on 'Fetch Remote Config' button.
+  [_app.buttons[@"Fetch Remote Config"] tap];
+
+  // Verify that the Remote Config value is visible.
+  XCUIElement* config = [_app staticTexts][@"WELCOME TO MY AWESOME APP!"];
+  FIRWaitForVisibleWithTimeout(config, 20);
+  XCTAssertTrue(config.exists);
 }
 
 @end

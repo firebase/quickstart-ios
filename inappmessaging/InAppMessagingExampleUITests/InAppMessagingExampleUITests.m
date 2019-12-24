e8fb46882fb8ff505053ae7504359d2c36df03c5
Paul Beusterien <paulbeusterien@google.com>
2019-12-23 16:46:20 -0800

    Update FCM and FIAM UITests

--- inappmessaging/InAppMessagingExampleUITests/InAppMessagingExampleUITests.m
+++ inappmessaging/InAppMessagingExampleUITests/InAppMessagingExampleUITests.m
@@ -1,9 +1,16 @@
+//  Copyright (c) 2019 Google Inc.
 //
-//  InAppMessagingExampleUITests.m
-//  InAppMessagingExampleUITests
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//  Created by Morgan Chen on 8/9/18.
-//  Copyright © 2018 Firebase. All rights reserved.
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <XCTest/XCTest.h>
@@ -12,29 +19,18 @@
 
 @end
 
-@implementation InAppMessagingExampleUITests
+@implementation InAppMessagingExampleUITests {
+  XCUIApplication *_app;
+}
 
 - (void)setUp {
-    [super setUp];
-    
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-    
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  [super setUp];
+  _app = [[XCUIApplication alloc] init];
+  [_app launch];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testVerifyAppLaunched {
+  XCTAssertTrue(_app.state == XCUIApplicationStateRunningForeground);
 }
 
 @end

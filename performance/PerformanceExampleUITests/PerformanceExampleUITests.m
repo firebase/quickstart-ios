a8b1df8990f7d13da15a5f8626fbe26ed40c5506
Paul Beusterien <paulbeusterien@google.com>
2019-12-25 08:16:53 -0800

    Perf UITest converge (#853)

--- performance/PerformanceExampleUITests/PerformanceExampleUITests.m
+++ performance/PerformanceExampleUITests/PerformanceExampleUITests.m
@@ -1,9 +1,17 @@
 //
-//  PerformanceExampleUITests.m
-//  PerformanceExampleUITests
+//  Copyright (c) 2019 Google Inc.
 //
-//  Created by Ibrahim Ulukaya on 2/13/18.
-//  Copyright © 2018 Google Inc. All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <XCTest/XCTest.h>
@@ -12,29 +20,22 @@
 
 @end
 
-@implementation PerformanceExampleUITests
+@implementation PerformanceExampleUITests{
+  XCUIApplication *_app;
+}
 
 - (void)setUp {
-    [super setUp];
-    
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-    
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  [super setUp];
+  _app = [[XCUIApplication alloc] init];
+  [_app launch];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+- (void)testVerifyAppLaunched {
+    // Verify that Performance Example app launched successfully and its title is visible.
+    XCTAssertTrue([_app navigationBars][@"Firebase Performance Example"].exists);
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Make sure Google logo is loaded.
+    XCTAssertTrue([[[_app images] elementBoundByIndex:0] exists]);
 }
 
 @end

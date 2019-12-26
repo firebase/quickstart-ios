d7f26c3f972c4f08200d2e486e2d4edbce0ecbdf
Paul Beusterien <paulbeusterien@google.com>
2019-12-26 07:03:43 -0800

    Update FCM and FIAM UITests (#851)

--- messaging/MessagingExampleUITests/MessagingExampleUITests.m
+++ messaging/MessagingExampleUITests/MessagingExampleUITests.m
@@ -1,40 +1,89 @@
+//  Copyright (c) 2019 Google Inc.
 //
-//  MessagingExampleUITests.m
-//  MessagingExampleUITests
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//  Created by Ibrahim Ulukaya on 2/13/18.
-//  Copyright © 2018 Google Inc. All rights reserved.
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <XCTest/XCTest.h>
 
-@interface MessagingExampleUITests : XCTestCase
+#import "FIREGHelper.h"
 
+typedef BOOL (^SystemAlertHandler)(XCUIElement *);
+static SystemAlertHandler const alertHandler = ^(XCUIElement *element) {
+  if (element.buttons[@"OK"].exists) {
+    [element.buttons[@"OK"] tap];
+  }
+  if (element.buttons[@"Allow"].exists) {
+    [element.buttons[@"Allow"] tap];
+  }
+  return YES;
+};
+
+static NSString *const subscribeButton = @"Subscribe To Weather";
+static NSString *const logButton = @"Log Token";
+
+@interface MessagingExampleUITests : XCTestCase
 @end
 
-@implementation MessagingExampleUITests
+@implementation MessagingExampleUITests {
+  XCUIApplication *_app;
+  id<NSObject> notificationPermissionMonitor;
+}
 
 - (void)setUp {
-    [super setUp];
-    
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-    
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  [super setUp];
+  _app = [[XCUIApplication alloc] init];
+  notificationPermissionMonitor =
+      [self addUIInterruptionMonitorWithDescription:@"Allow Notifications" handler:alertHandler];
+  [_app launch];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+  [self removeUIInterruptionMonitor:notificationPermissionMonitor];
+  [super tearDown];
 }
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testVerifyAppLaunched {
+  FIRWaitTillAlertPresent(defaultTimeout);
+  // Remove the permission request and make sure it's gone.
+  [[[_app navigationBars] element] tap];
+  FIRWaitTillAlertGone(defaultTimeout);
+
+  // Verify that Messaging Example app launched successfully and its title is visible.
+  XCTAssertTrue(_app.buttons[subscribeButton].exists);
+
+  // Make sure FCM token has been received. It's index 1 after the title at index 0.
+  XCTAssertTrue(
+      [[[[_app staticTexts] elementBoundByIndex:1] label] containsString:@"Received FCM token"]);
+
+  // Tap on "Subscribe to News" button.
+  [_app.buttons[subscribeButton] tap];
+
+  // Tap on "Log Token" button.
+  [_app.buttons[logButton] tap];
+
+  // Make sure app is still in foreground (i.e. not crashed).
+  XCTAssertTrue(_app.buttons[subscribeButton].exists);
+
+  // Wait till network call is finished.
+  NSPredicate *gone = [NSPredicate predicateWithFormat:@"exists == false"];
+  FIRWaitForPredicateWithTimeout(gone, [_app staticTexts][@"No InstanceID token"], 20);
+
+  // Make sure [FIRMessaging messaging].FCMToken returns FCM token.
+  XCTAssertTrue(
+      [[[[_app staticTexts] elementBoundByIndex:1] label] containsString:@"Logged FCM token"]);
+  // Make sure [FIRInstanceID instanceID] instanceIDWithHandler: returns FCM token.
+  XCTAssertTrue([[[[_app staticTexts] elementBoundByIndex:2] label]
+      containsString:@"Remote InstanceID token"]);
 }
 
 @end

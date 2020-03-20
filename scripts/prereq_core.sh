0bfdab5abe56ae590f2d01fbf593ea7da7130844
Paul Beusterien <paulbeusterien@google.com>
2020-03-20 09:55:26 -0700

    only do info_script for gha

--- scripts/prereq_core.sh
+++ scripts/prereq_core.sh
@@ -13,14 +13,17 @@
 # limitations under the License.
 
 # Set have_secrets to true or false.
-. ../scripts/check_secrets.sh
 
 ruby ../scripts/info_script.rb "${PROJECT}"
-if [[ "$have_secrets" == true ]]; then
-  ../scripts/install_secrets.sh
-  cp Secrets/quickstart-ios/"${DIRECTORY}"/GoogleService-Info.plist ./
-  cp Secrets/quickstart-ios/TestUtils/FIREGSignInInfo.h ../TestUtils/
-else
-  cp ../mock-GoogleService-Info.plist ./GoogleService-Info.plist
-  sed -i '' "/<key>BUNDLE_ID</{n;s/id/com.google.firebase.quickstart.${PROJECT}Example/;}" GoogleService-Info.plist
+
+if [[ -n "${TRAVIS_PULL_REQUEST:-}" ]]; then
+  . ../scripts/check_secrets.sh
+  if [[ "$have_secrets" == true ]]; then
+    ../scripts/install_secrets.sh
+    cp Secrets/quickstart-ios/"${DIRECTORY}"/GoogleService-Info.plist ./
+    cp Secrets/quickstart-ios/TestUtils/FIREGSignInInfo.h ../TestUtils/
+  else
+    cp ../mock-GoogleService-Info.plist ./GoogleService-Info.plist
+    sed -i '' "/<key>BUNDLE_ID</{n;s/id/com.google.firebase.quickstart.${PROJECT}Example/;}" GoogleService-Info.plist
+  fi
 fi

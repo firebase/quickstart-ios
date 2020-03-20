1b1c0d431fad80f64bfb05a4bba86567f5d049d8
Paul Beusterien <paulbeusterien@google.com>
2020-03-20 10:21:26 -0700

    restore xcpretty

--- scripts/test.sh
+++ scripts/test.sh
@@ -34,7 +34,7 @@ if [[ "$have_secrets" == true ]]; then
       build \
       test \
       ONLY_ACTIVE_ARCH=YES \
-      ) || EXIT_STATUS=$?
+      | xcpretty) || EXIT_STATUS=$?
 else
     # Skip running tests if GoogleService-Info.plist's weren't decoded.
     (xcodebuild \

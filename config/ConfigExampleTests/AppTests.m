dc80776acb7903a4fae71eff81b3f99a3a1a7706
Zongran Luo <ollkorrect999@gmail.com>
2020-05-26 10:43:27 -0700

    Revert a file to head.

--- config/ConfigExampleTests/AppTests.m
+++ config/ConfigExampleTests/AppTests.m
@@ -17,8 +17,8 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-#import "FirebaseCore/FIRApp.h"
-#import "FirebaseRemoteConfig/FIRRemoteConfig.h"
+#import "FIRApp.h"
+#import "FIRRemoteConfig.h"
 
 @interface AppTests : XCTestCase
 @end

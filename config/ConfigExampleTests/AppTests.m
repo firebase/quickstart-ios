68632b8353b203cbdc47202a8d73fe74a0dccb1b
Zongran Luo <ollkorrect999@gmail.com>
2020-05-28 15:06:22 -0700

    Determine if a FIRApp exists before configuration.

--- config/ConfigExampleTests/AppTests.m
+++ config/ConfigExampleTests/AppTests.m
@@ -17,8 +17,8 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-#import "FIRApp.h"
-#import "FIRRemoteConfig.h"
+#import "FirebaseCore/FIRApp.h"
+#import "FirebaseRemoteConfig/FIRRemoteConfig.h"
 
 @interface AppTests : XCTestCase
 @end
@@ -27,7 +27,7 @@
 
 
 - (void)testRemoteConfig {
-  [FIRApp configure];
+  if([FIRApp defaultApp] == nil){ [FIRApp configure]; };
   FIRRemoteConfig *remoteConfig = [FIRRemoteConfig remoteConfig];
   XCTAssert([remoteConfig isKindOfClass:[FIRRemoteConfig class]]);
 }

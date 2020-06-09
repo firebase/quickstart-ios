bbe456e25d044880a7f8cb5447668b967ad1ffb2
Zongran Luo <ollkorrect999@gmail.com>
2020-06-08 17:14:52 -0700

    update blank from tab to white spaces.

--- scripts/framework_test.sh
+++ scripts/framework_test.sh
@@ -31,21 +31,21 @@ cd "${SAMPLE}"
 
 if [[ "$have_secrets" == true ]]; then
     (
-		xcodebuild \
-		-project ${SAMPLE}Example.xcodeproj \
-		-scheme  ${SAMPLE}Example${SWIFT_SUFFIX} \
-		-destination 'platform=iOS Simulator,name=iPhone 11 Pro' "OTHER_LDFLAGS=\$(OTHER_LDFLAGS) -ObjC" "FRAMEWORK_SEARCH_PATHS= \$(PROJECT_DIR)/Firebase/" HEADER_SEARCH_PATHS='$(PROJECT_DIR)/Firebase' \
-      build \
-      test \
+    xcodebuild \
+    -project ${SAMPLE}Example.xcodeproj \
+    -scheme  ${SAMPLE}Example${SWIFT_SUFFIX} \
+    -destination 'platform=iOS Simulator,name=iPhone 11 Pro' "OTHER_LDFLAGS=\$(OTHER_LDFLAGS) -ObjC" "FRAMEWORK_SEARCH_PATHS= \$(PROJECT_DIR)/Firebase/" HEADER_SEARCH_PATHS='$(PROJECT_DIR)/Firebase' \
+    build \
+    test \
     ) || EXIT_STATUS=$?
 else
     # Skip running tests if GoogleService-Info.plist's weren't decoded.
     (
-		xcodebuild \
-		-project ${SAMPLE}Example.xcodeproj \
-		-scheme  ${SAMPLE}Example${SWIFT_SUFFIX} \
-		-destination 'platform=iOS Simulator,name=iPhone 11 Pro' "OTHER_LDFLAGS=\$(OTHER_LDFLAGS) -ObjC" "FRAMEWORK_SEARCH_PATHS= \$(PROJECT_DIR)/Firebase/" HEADER_SEARCH_PATHS='$(PROJECT_DIR)/Firebase' \
-      build
+    xcodebuild \
+    -project ${SAMPLE}Example.xcodeproj \
+    -scheme  ${SAMPLE}Example${SWIFT_SUFFIX} \
+    -destination 'platform=iOS Simulator,name=iPhone 11 Pro' "OTHER_LDFLAGS=\$(OTHER_LDFLAGS) -ObjC" "FRAMEWORK_SEARCH_PATHS= \$(PROJECT_DIR)/Firebase/" HEADER_SEARCH_PATHS='$(PROJECT_DIR)/Firebase' \
+    build
     ) || EXIT_STATUS=$?
 fi
 

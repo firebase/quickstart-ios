fd1b52ff3ef85f7f55f1b7c1d043a47e674254c8
Nick Cooke <ncooke3@gatech.edu>
2020-06-30 08:54:56 -0400

    Added base tests

--- config/ConfigExampleTests/ConfigExampleTests.swift
+++ config/ConfigExampleTests/ConfigExampleTests.swift
@@ -17,20 +17,20 @@ import XCTest
 
 class ConfigExampleTests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.

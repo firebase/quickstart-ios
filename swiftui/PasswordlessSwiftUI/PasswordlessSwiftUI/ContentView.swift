90c701b151849baa771a5a8c7985f37f886271e6
Nick Cooke <ncooke3@gatech.edu>
2020-07-21 19:01:58 -0400

    Resolved feedback

--- swiftui/PasswordlessSwiftUI/PasswordlessSwiftUI/ContentView.swift
+++ swiftui/PasswordlessSwiftUI/PasswordlessSwiftUI/ContentView.swift
@@ -1,9 +1,16 @@
+// Copyright 2020 Google LLC
 //
-//  ContentView.swift
-//  PasswordlessSwiftUI
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Copyright © 2020 Google LLC. All rights reserved.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import SwiftUI
 import Firebase
@@ -12,72 +19,83 @@ import Firebase
 /// Main view where user can login in using Email Link Authentication.
 struct ContentView: View {
   @State private var email: String = ""
-  @State private var presentSheet = false
+  @State private var isPresentingSheet = false
 
   var body: some View {
 
     NavigationView {
-      VStack(spacing: 50.0) {
+      VStack(alignment: .leading) {
         Text("Authenticate users with only their email, no password required!")
-          .frame(maxWidth: .infinity, alignment: .leading)
-        
-        Spacer()
-          .frame(height: 0.0)
-        
+          .padding(.bottom, 60)
+
         CustomStyledTextField(
-          text: $email, placeholder: "Email", symbol: "person.circle.fill"
+          text: $email, placeholder: "Email", symbolName: "person.circle.fill"
         )
 
         CustomStyledButton(title: "Send Sign In Link") {
-          sendSignInLink(to: email)
+          sendSignInLink()
         }
+        .disabled(email.isEmpty)
 
         Spacer()
       }
       .padding()
       .navigationBarTitle("Passwordless Login")
-
     }
     .onOpenURL { (url) in
       let link = url.absoluteString
       if Auth.auth().isSignIn(withEmailLink: link) {
-        passwordlessSignIn(email: email, link: link) { (success) in
-          self.presentSheet = success
+        passwordlessSignIn(email: email, link: link) { user, error in
+          isPresentingSheet = user?.isEmailVerified ?? false
         }
       }
     }
-    .sheet(isPresented: $presentSheet) {
+    .sheet(isPresented: $isPresentingSheet) {
       SuccessView(email: $email)
     }
-
   }
 
+  // MARK: - Firebase 🔥
+
+  private func sendSignInLink() {
+    let actionCodeSettings = ActionCodeSettings()
+    actionCodeSettings.url = URL(string: "https://passwordlessswiftui.page.link/demo_login")
+    actionCodeSettings.handleCodeInApp = true
+    actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+
+    Auth.auth().sendSignInLink(toEmail: email, actionCodeSettings: actionCodeSettings)
+  }
+
+  private func passwordlessSignIn(email: String, link: String,
+                                  completion: @escaping (_ user: User?, _ error: Error?) -> ()) {
+    Auth.auth().signIn(withEmail: email, link: link) { result, error in
+      print(error ?? "Authentication was successful.")
+      let newUser = error == nil ? Auth.auth().currentUser : nil
+      completion(newUser, error)
+    }
+  }
 }
 
 /// A custom styled TextField with an SF symbol icon.
 struct CustomStyledTextField: View {
   @Binding var text: String
   let placeholder: String
-  let symbol: String
+  let symbolName: String
   
   var body: some View {
     HStack {
-      Spacer()
-        .frame(width: 15.0)
-
-      Image(systemName: symbol)
+      Image(systemName: symbolName)
         .imageScale(.large)
+        .padding(.leading)
       
       TextField(placeholder, text: $text)
-        .padding([.top, .bottom])
+        .padding(.vertical)
         .accentColor(.orange)
+        .autocapitalization(.none)
     }
-    .autocapitalization(.none)
     .background(
       RoundedRectangle(cornerRadius: 16.0, style: .circular)
-      .foregroundColor(
-        Color(.secondarySystemFill)
-      )
+      .foregroundColor(Color(.secondarySystemFill))
     )
   }
 }
@@ -88,10 +106,13 @@ struct CustomStyledButton: View {
   let action: () -> Void
 
   var body: some View {
-    Button(title, action: action)
-    .padding()
-    .frame(maxWidth: .infinity)
-    .accentColor(.white)
+    Button(action: action) {
+      Spacer()
+      Text(title)
+        .padding()
+        .accentColor(.white)
+      Spacer()
+    }
     .background(Color.orange)
     .cornerRadius(16.0)
   }
@@ -103,56 +124,32 @@ struct SuccessView: View {
   @Binding var email: String
   
   var body: some View {
+    /// The first view in this `ZStack` is a `Color` view that expands
+    /// to set the background color of the `SucessView`.
     ZStack {
       Color.orange
         .edgesIgnoringSafeArea(.all)
         
-      VStack {
+      VStack(alignment: .leading) {
         Group {
           Text("Welcome")
             .font(.largeTitle)
             .fontWeight(.semibold)
-            .frame(maxWidth: .infinity, alignment: .leading)
           
           Text(email.lowercased())
             .font(.title3)
             .fontWeight(.bold)
             .multilineTextAlignment(.leading)
-            .frame(maxWidth: .infinity, alignment: .leading)
         }
         .padding(.leading)
         
         Image(systemName: "checkmark.circle")
           .resizable()
           .aspectRatio(contentMode: .fit)
-          .scaleEffect(0.5, anchor: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+          .scaleEffect(0.5)
       }
       .foregroundColor(.white)
     }
-  }
-}
-
-
-// MARK: - Firebase 🔥
-
-private func sendSignInLink(to email: String) {
-  let actionCodeSettings = ActionCodeSettings()
-  actionCodeSettings.url = URL(string: "https://passwordlessswiftui.page.link/demo_login")
-  actionCodeSettings.handleCodeInApp = true
-  actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
-
-  Auth.auth().sendSignInLink(toEmail: email, actionCodeSettings: actionCodeSettings)
-}
-
-private func passwordlessSignIn(email: String, link: String,
-                                completion: @escaping (_ success: Bool)->()) {
-  Auth.auth().signIn(withEmail: email, link: link) { result, error in
-    guard error == nil, let currentUser = Auth.auth().currentUser else {
-      completion(false)
-      return print(error!)
-    }
-
-    completion(currentUser.isEmailVerified)
   }
 }
 
@@ -166,16 +163,17 @@ struct ContentView_Previews: PreviewProvider {
 
 extension View {
   func handlePasswordlessLogin(forEmail email: String,
-                               completion: @escaping (Bool)->()) -> some View {
+                               completion: @escaping (_ user: User?, _ error: Error?) -> ()) -> some View {
       self.onOpenURL { (url) in
         let link = url.absoluteString
         if Auth.auth().isSignIn(withEmailLink: link) {
-          passwordlessSignIn(email: email, link: link) { (success) in
-            completion(success)
+          Auth.auth().signIn(withEmail: email, link: link) { result, error in
+            print(error ?? "Authentication was successful.")
+            let newUser = error == nil ? Auth.auth().currentUser : nil
+            completion(newUser, error)
           }
-        } else {
-          completion(false)
         }
       }
     }
 }
+

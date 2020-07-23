4461e68335bda76ec6d268233a3343a21c99782b
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2020-07-23 17:42:09 -0400

    Remove unneeded comment

--- analytics/AnalyticsExample/AppDelegate.swift
+++ analytics/AnalyticsExample/AppDelegate.swift
@@ -20,7 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication
                      .LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
     FirebaseApp.configure()
     return true
   }

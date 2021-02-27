44826ce951cd922e3bc43da1bce1c2bd6af239aa
Rachel Collins <rdcollins15@gmail.com>
2021-02-26 16:57:25 -0800

    adding FirestoreSwiftUIExample app to the firestore project

---
+++ firestore/FirestoreSwiftUIExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FirestoreSwiftUIExample
+//
+//  Created by Rachel Collins on 2/26/21.
+//  Copyright © 2021 Firebase. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firestore/FirestoreSwiftUIExample/FirestoreSwiftUIExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirestoreSwiftUIExampleApp.swift
+//  FirestoreSwiftUIExample
+//
+//  Created by Rachel Collins on 2/26/21.
+//  Copyright © 2021 Firebase. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct FirestoreSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

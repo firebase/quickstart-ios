10aecdcb700f8e8762ba0cc46ca7ab0d5f0b082c
Rachel Collins <rdcollins15@gmail.com>
2021-03-01 15:42:24 -0800

    adding Firebase to new app

--- firestore/FirestoreSwiftUIExample/FirestoreSwiftUIExampleApp.swift
+++ firestore/FirestoreSwiftUIExample/FirestoreSwiftUIExampleApp.swift
@@ -7,9 +7,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirestoreSwiftUIExampleApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()

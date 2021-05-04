0ac9db1e14ea4797a1725d8fb872918de8525746
Morgan Chen <morganchen12@gmail.com>
2021-05-04 14:40:36 -0700

    Update quickstart to align with quickstart-js

--- functions/FunctionsExampleSwift/CommentCell.swift
+++ functions/FunctionsExampleSwift/CommentCell.swift
@@ -42,7 +42,7 @@ class CommentCell: MDCCollectionViewCell {
         // [END_EXCLUDE]
       }
       // [END function_error]
-      if let text = result?.data as? String {
+      if let text = result?.data["text"] as? String {
         self.resultField.text = text
       }
     }

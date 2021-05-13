e168511611765b8eddcb9efba6217171a0348aba
Morgan Chen <morganchen12@gmail.com>
2021-05-13 13:37:11 -0700

    Revert #1169

--- scripts/prereq_core.sh
+++ scripts/prereq_core.sh
@@ -20,8 +20,8 @@ if [[ -n "${GITHUB_WORKFLOW:-}" ]]; then
   . ../scripts/check_secrets.sh
   if [[ "$have_secrets" == true ]]; then
     ../scripts/install_secrets.sh
-    cp Secrets/"${DIRECTORY}"/GoogleService-Info.plist ./
-    cp Secrets/TestUtils/FIREGSignInInfo.h ../TestUtils/
+    cp Secrets/quickstart-ios/"${DIRECTORY}"/GoogleService-Info.plist ./
+    cp Secrets/quickstart-ios/TestUtils/FIREGSignInInfo.h ../TestUtils/
   else
     cp ../mock-GoogleService-Info.plist ./GoogleService-Info.plist
     sed -i '' "/<key>BUNDLE_ID</{n;s/id/com.google.firebase.quickstart.${PROJECT}Example/;}" GoogleService-Info.plist

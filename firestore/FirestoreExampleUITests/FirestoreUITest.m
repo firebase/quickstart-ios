bd2eb9da55709ea40131cf8a6f471ad8538fde65
Paul Beusterien <paulbeusterien@google.com>
2021-07-02 11:26:53 -0700

    Fix race in Firestore UI Tests (#1207)

--- firestore/FirestoreExampleUITests/FirestoreUITest.m
+++ firestore/FirestoreExampleUITests/FirestoreUITest.m
@@ -28,24 +28,28 @@ static NSString *const testPassword = @"Test123";
 @interface FirestoreUITest : XCTestCase
 @end
 
-@implementation FirestoreUITest {
-  XCUIApplication *_app;
+static XCUIApplication *sApp;
+
+@implementation FirestoreUITest
+
++ (void)setUp {
+  sApp = [[XCUIApplication alloc] init];
+  [sApp launch];
+  if (![self isSignedIn]) {
+    [self doSignIn];
+  }
+  
+  // The next two navigations are to give the restaurants time to load.  Is there a better way?
+
+  // Navigate to the filter screen.
+  [sApp.buttons[@"Filter"] tap];
+
+  // Navigate back.
+  [sApp.buttons[@"Done"] tap];
 }
 
 - (void)setUp {
   [super setUp];
-  _app = [[XCUIApplication alloc] init];
-  [_app launch];
-  if (![self isSignedIn]) {
-    [self doSignIn];
-  }
-  // The next two navigations are to give the restaurants time to load.  Is there a better way?
-
-  // Navigate to the filter screen.
-  [_app.buttons[@"Filter"] tap];
-
-  // Navigate back.
-  [_app.buttons[@"Done"] tap];
 
   // Make sure table with restaurants has loaded.
   NSUInteger allRestaurants = [self restaurantsCount];
@@ -61,31 +65,31 @@ static NSString *const testPassword = @"Test123";
 
 - (void)testAddComment {
   // Navigate to the first restaurant in the list.
-  FIRWaitForVisible(_app.navigationBars[@"Friendly Eats"]);
-  XCUIElement *firstRestaurant = [_app.cells firstMatch];
+  FIRWaitForVisible(sApp.navigationBars[@"Friendly Eats"]);
+  XCUIElement *firstRestaurant = [sApp.cells firstMatch];
   [firstRestaurant tap];
 
   // Navigate to the review section.
-  FIRWaitForVisible(_app.buttons[@"Add"]);
-  [_app.buttons[@"Add"] tap];
-  FIRWaitForVisible(_app.navigationBars[@"Write a Review"]);
+  FIRWaitForVisible(sApp.buttons[@"Add"]);
+  [sApp.buttons[@"Add"] tap];
+  FIRWaitForVisible(sApp.navigationBars[@"Write a Review"]);
 
   // If we have 5 stars in the Imageview, the last, 5th star will be somewhere between 80% and 100%
   // of image width, so 90% is just the horizontal middle of it.
-  XCUIElement *stars = _app.otherElements[starRating];
+  XCUIElement *stars = sApp.otherElements[starRating];
   [[stars coordinateWithNormalizedOffset:(CGVectorMake(0.9, 0.5))] tap];
 
   // Type the comment into text field.
-  XCUIElement *textField = [[_app textFields] firstMatch];
+  XCUIElement *textField = [[sApp textFields] firstMatch];
   [textField tap];
   [textField typeText:@"The food was delicious!"];
 
   // Navigate back to main screen.
   NSArray *backButtons = @[ @"Done", @"Friendly Eats" ];
   for (id button in backButtons) {
-    FIRWaitForVisibleWithTimeout(_app.buttons[button], 20);
-    [_app.buttons[button] tap];
-    FIRWaitForVisibleWithTimeout([[_app tables] firstMatch], 20);
+    FIRWaitForVisibleWithTimeout(sApp.buttons[button], 20);
+    [sApp.buttons[button] tap];
+    FIRWaitForVisibleWithTimeout([[sApp tables] firstMatch], 20);
   }
   [self checkMainScreenIsDisplayed];
 }
@@ -93,15 +97,15 @@ static NSString *const testPassword = @"Test123";
 // TODO(b/147740878): reenable this test once we've figured out how to make it deterministic
 - (void)xtestApplyFilter {
   // Navigate to the filter screen.
-  [_app.buttons[@"Filter"] tap];
+  [sApp.buttons[@"Filter"] tap];
 
   // Filter by category.
-  FIRWaitForVisible(_app.textFields[@"Category"]);
-  [_app.textFields[@"Category"] tap];
+  FIRWaitForVisible(sApp.textFields[@"Category"]);
+  [sApp.textFields[@"Category"] tap];
 
   // Intract with wheel picker and apply the filter,
-  [[[_app pickerWheels] element] adjustToPickerWheelValue:@"Deli"];
-  [[_app buttons][@"Done"] tap];
+  [[[sApp pickerWheels] element] adjustToPickerWheelValue:@"Deli"];
+  [[sApp buttons][@"Done"] tap];
 
   [self checkMainScreenIsDisplayed];
   // Check that filter is applied.
@@ -110,25 +114,25 @@ static NSString *const testPassword = @"Test123";
 }
 
 - (NSUInteger)restaurantsCount {
-  return [[[[_app tables] firstMatch] cells] count];
+  return [[[[sApp tables] firstMatch] cells] count];
 }
 
-- (void)doSignIn {
-  XCUIElementQuery *buttons = [_app buttons];
++ (void)doSignIn {
+  XCUIElementQuery *buttons = [sApp buttons];
   [buttons [@"Sign in with email"] tap];
   // Type test email.
-  XCUIElement *email = _app.textFields[@"Enter your email"];
+  XCUIElement *email = sApp.textFields[@"Enter your email"];
   [email tap];
   [email typeText:testEmail];
   [[buttons [@"Next"] firstMatch] tap];
 
-  XCUIElement *name =_app.textFields[@"First & last name"];
+  XCUIElement *name =sApp.textFields[@"First & last name"];
   // This account does not exist before. First & last name is required.
   if (name.exists) {
     [name tap];
     [name typeText:testName];
     // Type test password.
-    XCUIElement *password = _app.secureTextFields[@"Choose password"];
+    XCUIElement *password = sApp.secureTextFields[@"Choose password"];
 
     [password tap];
     [password typeText:testPassword];
@@ -137,7 +141,7 @@ static NSString *const testPassword = @"Test123";
   // This account existed and only password required.
   else{
     // Type test password.
-    XCUIElement *password = _app.secureTextFields[@"Enter your password"];
+    XCUIElement *password = sApp.secureTextFields[@"Enter your password"];
 
     [password tap];
     [password typeText:testPassword];
@@ -147,18 +151,18 @@ static NSString *const testPassword = @"Test123";
 
 
   // Wait till list with restaurants is loaded.
-  FIRWaitForVisible([[_app cells] firstMatch]);
+  FIRWaitForVisible([[sApp cells] firstMatch]);
 }
 
-- (BOOL)isSignedIn {
-  return ![_app buttons][@"Sign in with email"].exists;
++ (BOOL)isSignedIn {
+  return ![sApp buttons][@"Sign in with email"].exists;
 }
 
 - (void)checkMainScreenIsDisplayed {
   // Check that navigation bar contains all the required items.
   NSArray<NSString *> *navBar = @[ @"Populate", @"Filter" ];
   for (NSString *button in navBar) {
-    XCTAssertTrue(_app.buttons[button].exists);
+    XCTAssertTrue(sApp.buttons[button].exists);
   }
 }
 

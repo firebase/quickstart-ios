ee3e0cd036d1692733cb683be766c0983a639a25
Jesus Crespo <jesuscrespo@google.com>
2021-07-07 12:25:30 -0500

    Fix directory naming and formatting

--- abtesting/Tests_iOS/Tests_iOS.swift
+++ abtesting/Tests_iOS/Tests_iOS.swift
@@ -17,7 +17,6 @@
 import XCTest
 
 class Tests_iOS: XCTestCase {
-
   override func setUpWithError() throws {
     // Put setup code here. This method is called before the invocation of each test method in
     // the class.

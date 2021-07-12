f258fc8640d286bf94fd4527fa52ec1460e56ba3
Judy Liu <lexuan@google.com>
2021-07-12 14:37:03 -0700

    Dismiss "New Post" view once user taps post button

--- database/DatabaseExampleSwiftUI/DatabaseExample/Shared/Views/NewPostsView.swift
+++ database/DatabaseExampleSwiftUI/DatabaseExample/Shared/Views/NewPostsView.swift
@@ -1,14 +1,24 @@
 //
-//  NewPostsView.swift
-//  DatabaseExample
+//  Copyright (c) 2021 Google Inc.
 //
-//  Created by Judy Liu on 7/7/21.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import SwiftUI
 
 struct NewPostsView: View {
-  @StateObject var user = UserViewModel()
+  @ObservedObject var user = UserViewModel()
+  @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
   @State private var newPostTitle: String = ""
   @State private var newPostBody: String = ""
   @State private var placeholderText: String = "Say something..."
@@ -39,6 +49,7 @@ struct NewPostsView: View {
                               title: newPostTitle,
                               body: newPostBody
                             )
+                            self.presentationMode.wrappedValue.dismiss()
                           }) {
                             Text("Post")
                           })

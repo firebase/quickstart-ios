8f6dfc67e02a05fa895ea525d05364033ecb6b28
Jesus Crespo <jesuscrespo@google.com>
2021-07-12 18:21:09 -0500

    Add async/await functionality to abtesting [SwiftUI] (#1214)
    * Add async/await functionality
    
    Use updateFromRemoteConfigAsync for iOS 15
    
    * Update abtesting.yml with build before test
    
    * Fix backward-compatibility

--- abtesting/Shared/AppConfig.swift
+++ abtesting/Shared/AppConfig.swift
@@ -57,6 +57,33 @@ class AppConfig: ObservableObject {
     }
   }
 
+  #if swift(>=5.5)
+    @available(iOS 15, *)
+    func updateFromRemoteConfigAsync() async {
+      let remoteConfig = RemoteConfig.remoteConfig()
+      do {
+        let status = try await remoteConfig.fetch(withExpirationDuration: 0)
+        print("Config fetch completed with status: \(status.debugDescription)")
+        do {
+          let changed = try await remoteConfig.activate()
+          let value = remoteConfig["color_scheme"].stringValue ?? "nil"
+          if changed {
+            print("Remote Config changed to: \(value)")
+            DispatchQueue.main.async {
+              self.colorScheme = ColorScheme(value)
+            }
+          } else {
+            print("Remote Config did not change from: \(value)")
+          }
+        } catch {
+          print("Error activating config: \(error)")
+        }
+      } catch {
+        print("Error fetching config: \(error)")
+      }
+    }
+  #endif
+
   @objc func printInstallationAuthToken() {
     Installations.installations().authTokenForcingRefresh(true) { token, error in
       if let error = error {

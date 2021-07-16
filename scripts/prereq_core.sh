bdb11b3c2b7abcb0c4981bdf1aacc17da81d933a
granluo <granluo@google.com>
2021-07-16 14:11:51 -0700

    Add mock plist to all SDKs.

--- scripts/prereq_core.sh
+++ scripts/prereq_core.sh
@@ -12,16 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ruby ../scripts/info_script.rb "${PROJECT}" "${LEGACY}"
-
 if [[ -n "${GITHUB_WORKFLOW:-}" ]]; then
   . ../scripts/check_secrets.sh
   if [[ "$have_secrets" == true ]]; then
     ../scripts/install_secrets.sh
     cp Secrets/quickstart-ios/"${DIRECTORY}"/GoogleService-Info.plist ./
     cp Secrets/quickstart-ios/TestUtils/FIREGSignInInfo.h ../TestUtils/
-  else
-    cp ../mock-GoogleService-Info.plist ./GoogleService-Info.plist
-    sed -i '' "/<key>BUNDLE_ID</{n;s/id/com.google.firebase.quickstart.${PROJECT}Example/;}" GoogleService-Info.plist
   fi
 fi

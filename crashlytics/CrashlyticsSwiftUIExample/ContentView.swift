f730cf5c61e8b0a04a313ba1c8630a01562135de
Judy Liu <lexuan@google.com>
2021-07-29 17:02:14 -0700

    add missing files for tvOS target

---
+++ crashlytics/CrashlyticsSwiftUIExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CrashlyticsSwiftUIExample
+//
+//  Created by Judy Liu on 7/29/21.
+//  Copyright © 2021 Google Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ crashlytics/CrashlyticsSwiftUIExample/CrashlyticsSwiftUIExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  CrashlyticsSwiftUIExampleApp.swift
+//  CrashlyticsSwiftUIExample
+//
+//  Created by Judy Liu on 7/29/21.
+//  Copyright © 2021 Google Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CrashlyticsSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

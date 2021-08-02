aceb34e4ec7618e22a345e4d40939f1df2f59851
Jesus Crespo <jesuscrespo@google.com>
2021-08-02 15:25:53 -0500

    Fix copyright

--- performance/SwiftUIPerformanceQuickstart/PerformanceExample/Tests_tvOS/Tests_tvOS.swift
+++ performance/SwiftUIPerformanceQuickstart/PerformanceExample/Tests_tvOS/Tests_tvOS.swift
@@ -1,8 +1,17 @@
 //
-//  Tests_tvOS.swift
-//  Tests_tvOS
+//  Copyright 2021 Google LLC
 //
-//  Created by Jesus Crespo on 8/2/21.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import XCTest

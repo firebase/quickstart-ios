ea8d0366f118958e2e17d8cfbef0c793445f7f2a
Judy Liu <lexuan@google.com>
2021-08-12 08:04:51 -0700

    rename folders to pass style test

--- crashlytics/CrashlyticsSwiftUIExample_(watchOS)_Extension/NotificationView.swift
+++ crashlytics/CrashlyticsSwiftUIExample_(watchOS)_Extension/NotificationView.swift
@@ -17,13 +17,13 @@
 import SwiftUI
 
 struct NotificationView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+  var body: some View {
+    Text("Hello, World!")
+  }
 }
 
 struct NotificationView_Previews: PreviewProvider {
-    static var previews: some View {
-        NotificationView()
-    }
+  static var previews: some View {
+    NotificationView()
+  }
 }

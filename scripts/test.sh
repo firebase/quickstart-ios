3c6f6dea07b281b393dfb85a08294d4eb2e185fe
Jesus Crespo <jesuscrespo@google.com>
2021-08-23 13:18:02 -0500

    Consolidate Quickstart CI script (#1258)
    * Unify SPM, CocoaPods, Obj-C, Swift, and SwiftUI script
    
    * Update Quickstart workflows
    
    * Add catalyst support to script
    
    * Downgrade Config tests to macOS 10.15
    
    * Use macOS 11 for SPM Quickstarts
    
    * Disable tests for problematic Quickstarts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2019 Google
+# Copyright 2021 Google
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,41 +18,111 @@
 # Build the quickstart. If we're running on the main repo (not a fork), we
 # also run the tests along with the decoded GoogleService-Info.plist files.
 
-set -eo pipefail
-
-EXIT_STATUS=0
+set -euo pipefail
 
 # Set have_secrets to true or false.
-. ./scripts/check_secrets.sh
+have_secrets=false
+source scripts/check_secrets.sh
 
-workspace_path="${SAMPLE}/${SAMPLE}Example.xcworkspace"
+# Get Xcode version
+system=$(uname -s)
+case "$system" in
+  Darwin)
+    xcode_version=$(xcodebuild -version | head -n 1)
+    xcode_version="${xcode_version/Xcode /}"
+    xcode_major="${xcode_version/.*/}"
+    ;;
+  *)
+    xcode_major="0"
+    ;;
+esac
 
-if [[ ! -z "$LEGACY" ]]; then
-  workspace_path="${SAMPLE}/Legacy${SAMPLE}Quickstart/${SAMPLE}Example.xcworkspace"
+# Check Xcode version when testing watchOS
+if [[ "$TEST" == true && \
+      "$OS" == watchOS && \
+      "$xcode_major" -lt 13 && \
+      "$xcode_version" != "12.5.1" && \
+      "$xcode_version" != "12.5" ]]; then
+    echo "Xcode version does not yet supporting testing on watchOS"
+    exit 1
 fi
 
-if [[ "$have_secrets" == true ]]; then
-    (xcodebuild \
-      -workspace "$workspace_path" \
-      -scheme ${SAMPLE}Example${SWIFT_SUFFIX} \
-      -sdk iphonesimulator \
-      -destination 'platform=iOS Simulator,name=iPhone 11' \
-      build \
-      test \
-      ONLY_ACTIVE_ARCH=YES \
-      OTHER_SWIFT_FLAGS=${SWIFT_DEFINES} \
-      | xcpretty) || EXIT_STATUS=$?
+# Initialize flags
+flags=()
+
+# Set project / workspace
+if [[ "$SPM" == true ]];then
+    flags+=( -project "${DIR}/${SAMPLE}Example.xcodeproj" )
 else
-    # Skip running tests if GoogleService-Info.plist's weren't decoded.
-    (xcodebuild \
-      -workspace "$workspace_path" \
-      -scheme ${SAMPLE}Example${SWIFT_SUFFIX} \
-      -sdk iphonesimulator \
-      -destination 'platform=iOS Simulator,name=iPhone 11' \
-      build \
-      ONLY_ACTIVE_ARCH=YES \
-      OTHER_SWIFT_FLAGS=${SWIFT_DEFINES} \
-      | xcpretty) || EXIT_STATUS=$?
+    if [[ "$LEGACY" == true ]]; then
+        WORKSPACE="${SAMPLE}/Legacy${SAMPLE}Quickstart/${SAMPLE}Example.xcworkspace"
+    else
+        WORKSPACE="${SAMPLE}/${SAMPLE}Example.xcworkspace"
+    fi
+    flags+=( -workspace "$WORKSPACE" )
 fi
 
-  exit $EXIT_STATUS
+# Set scheme
+if [[ "$SPM" == true ]];then
+    SCHEME="${SAMPLE}Example (${OS})"
+else
+    SCHEME="${SAMPLE}Example${SWIFT_SUFFIX}"
+fi
+flags+=( -scheme "$SCHEME" )
+
+# Set destination
+if [[ "$OS" == iOS ]]; then
+    DESTINATION="platform=iOS Simulator,name=${DEVICE}"
+    flags+=( -destination "$DESTINATION" )
+elif [[ "$OS" == tvOS ]]; then
+    DESTINATION="platform=tvOS Simulator,name=${DEVICE}"
+    flags+=( -destination "$DESTINATION" )
+elif [[ "$OS" == macOS || "$OS" == catalyst ]]; then
+    DESTINATION="platform=macos"
+    flags+=( -destination "$DESTINATION" )
+elif [[ "$OS" == watchOS ]]; then
+    DESTINATION="platform=watchOS Simulator,name=${DEVICE}"
+else
+    echo "Unsupported OS: ${OS}"
+    exit 1
+fi
+
+# Add extra flags
+
+if [[ "$SAMPLE" == Config ]];then
+    flags+=( -configuration Debug )
+fi
+
+if [[ "$OS" == catalyst ]];then
+    flags+=(
+        ARCHS=x86_64
+        VALID_ARCHS=x86_64
+        SUPPORTS_MACCATALYST=YES
+        SUPPORTS_UIKITFORMAC=YES
+    )
+fi
+
+flags+=(
+    CODE_SIGNING_REQUIRED=NO
+    CODE_SIGNING_ALLOWED=NO
+    build
+)
+
+# Check whether to test on top of building
+message=""
+if [[ "$TEST" == true && "$have_secrets" == true ]]; then
+    flags+=( test )
+elif [[ "$TEST" == true ]]; then
+    message="Missing secrets: tests did not run."
+else
+    message="Tests did not run."
+fi
+
+function xcb() {
+    echo xcodebuild "$@"
+    xcodebuild "$@" | xcpretty
+}
+
+# Run xcodebuild
+xcb "${flags[@]}"
+echo "$message"

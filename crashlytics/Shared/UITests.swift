c056435172901ca9ff18770deaf8f4f0208a6010
Judy Liu <lexuan@google.com>
2021-08-30 12:19:57 -0700

    remove autogenerated comments and functions

--- crashlytics/Shared/UITests.swift
+++ crashlytics/Shared/UITests.swift
@@ -18,16 +18,7 @@ import XCTest
 
 class UITests: XCTestCase {
   override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
     continueAfterFailure = false
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-  }
-
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
   }
 
   func testStaticUI() throws {

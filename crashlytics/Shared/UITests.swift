25dd4f2440c7f12237cc41e74738ae4a09f20561
Judy Liu <lexuan@google.com>
2021-08-30 12:45:38 -0700

    remove auto comments

--- crashlytics/Shared/UITests.swift
+++ crashlytics/Shared/UITests.swift
@@ -22,13 +22,8 @@ class UITests: XCTestCase {
   }
 
   func testStaticUI() throws {
-    // UI tests must launch the application that they test.
     let app = XCUIApplication()
     app.launch()
-
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-
     #if !os(macOS)
       XCTAssertTrue(
         app.navigationBars["Crashlytics Example"].exists,

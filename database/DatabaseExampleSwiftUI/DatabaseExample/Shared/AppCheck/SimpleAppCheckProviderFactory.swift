a6d14f41e43b54d1b194d706716e990e3ee21f93
Maksym Malyhin <mmaksym@google.com>
2021-09-15 21:41:04 -0400

    Factory: print debug provider, DeviceCheck fallback

--- database/DatabaseExampleSwiftUI/DatabaseExample/Shared/AppCheck/SimpleAppCheckProviderFactory.swift
+++ database/DatabaseExampleSwiftUI/DatabaseExample/Shared/AppCheck/SimpleAppCheckProviderFactory.swift
@@ -19,13 +19,22 @@ import Firebase
 
 class SimpleAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
   func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
-    #if targetEnvironment(simulator)
+      #if targetEnvironment(simulator)
       // App Attest is not available on simulators.
       // Use a debug provider.
-      return AppCheckDebugProvider(app: app)
-    #else
-      // Use App Attest provider on real devices.
-      return AppAttestProvider(app: app)
-    #endif
+      let provider = AppCheckDebugProvider(app: app)
+
+      // Print only locally generated token to avoid a valid token leak on CI.
+      print("Firebase App Check debug token: \(provider?.localDebugToken() ?? "" )")
+
+      return provider
+      #else
+      if #available(iOS 14.0, *) {
+        // Use App Attest provider on real devices.
+        return AppAttestProvider(app: app)
+      } else {
+        return DeviceCheckProvider(app: app)
+      }
+      #endif
   }
 }

509bbec194b877f1f32fa24bc81f022b29e7d9fe
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2021-12-25 11:15:46 +0900

    Messaging use async for iOS13

--- messaging/MessagingExampleSwift/AppDelegate.swift
+++ messaging/MessagingExampleSwift/AppDelegate.swift
@@ -78,9 +78,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   // [START receive_message]
   func application(_ application: UIApplication,
-                   didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                   fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult)
-                     -> Void) {
+                   didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async -> UIBackgroundFetchResult {
     // If you are receiving a notification message while your app is in the background,
     // this callback will not be fired till the user taps on the notification launching the application.
     // TODO: Handle data of notification
@@ -96,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Print full message.
     print(userInfo)
 
-    completionHandler(UIBackgroundFetchResult.newData)
+    return UIBackgroundFetchResult.newData
   }
 
   // [END receive_message]
@@ -119,13 +117,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 }
 
 // [START ios_10_message_handling]
-@available(iOS 10, *)
+
 extension AppDelegate: UNUserNotificationCenterDelegate {
   // Receive displayed notifications for iOS 10 devices.
   func userNotificationCenter(_ center: UNUserNotificationCenter,
-                              willPresent notification: UNNotification,
-                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
-                                -> Void) {
+                              willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
     let userInfo = notification.request.content.userInfo
 
     // With swizzling disabled you must let Messaging know about the message, for Analytics
@@ -142,12 +138,11 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     print(userInfo)
 
     // Change this to your preferred presentation option
-    completionHandler([[.alert, .sound]])
+    return [[.alert, .sound]]
   }
 
   func userNotificationCenter(_ center: UNUserNotificationCenter,
-                              didReceive response: UNNotificationResponse,
-                              withCompletionHandler completionHandler: @escaping () -> Void) {
+                              didReceive response: UNNotificationResponse) async {
     let userInfo = response.notification.request.content.userInfo
 
     // [START_EXCLUDE]
@@ -162,8 +157,6 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 
     // Print full message.
     print(userInfo)
-
-    completionHandler()
   }
 }
 

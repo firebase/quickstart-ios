61ffc0ae04aacc788d225b64560349018daf0835
Gran Luo <granluo@google.com>
2022-04-02 00:35:37 -0700

    Test script.

--- scripts/info_script.rb
+++ scripts/info_script.rb
@@ -26,11 +26,11 @@ end
 project = Xcodeproj::Project.open(project_path)
 
 # Add a file to the project in the main group
-files = project.new_file(file_name)
+file = project.new_file(file_name)
 
 # Add the file to the all targets
 project.targets.each do |target|
-  target.add_file_references([files])
+  target.add_file_references([file])
 end
 
 # save project

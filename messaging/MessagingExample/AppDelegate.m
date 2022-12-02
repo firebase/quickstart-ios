bc91321b9826b8d4d562799c66de16a275731bcc
Paul Beusterien <paulbeusterien@google.com>
2022-12-02 09:07:17 -0900

    Remove pre-iOS 10 code from Messaging Quickstart (#1460)

--- messaging/MessagingExample/AppDelegate.m
+++ messaging/MessagingExample/AppDelegate.m
@@ -41,25 +41,15 @@ NSString *const kGCMMessageIDKey = @"gcm.message_id";
   // Register for remote notifications. This shows a permission dialog on first run, to
   // show the dialog at a more appropriate time move this registration accordingly.
   // [START register_for_notifications]
-  if ([UNUserNotificationCenter class] != nil) {
-    // iOS 10 or later
-    // For iOS 10 display notification (sent via APNS)
-    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-    UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
-        UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
-    [[UNUserNotificationCenter currentNotificationCenter]
-        requestAuthorizationWithOptions:authOptions
-        completionHandler:^(BOOL granted, NSError * _Nullable error) {
-          // ...
-        }];
-  } else {
-    // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
-    UIUserNotificationType allNotificationTypes =
-    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
-    UIUserNotificationSettings *settings =
-    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
-    [application registerUserNotificationSettings:settings];
-  }
+
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+  UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+      UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+  [[UNUserNotificationCenter currentNotificationCenter]
+      requestAuthorizationWithOptions:authOptions
+      completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        // ...
+      }];
 
   [application registerForRemoteNotifications];
   // [END register_for_notifications]

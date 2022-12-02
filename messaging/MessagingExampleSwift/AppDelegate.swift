d197de0bec7f6d785ce449ffae472f483bda4d89
Paul Beusterien <paulbeusterien@google.com>
2022-12-02 06:57:32 -0900

    Remove pre-iOS 10 code from Messaging Quickstart

--- messaging/MessagingExampleSwift/AppDelegate.swift
+++ messaging/MessagingExampleSwift/AppDelegate.swift
@@ -37,20 +37,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Register for remote notifications. This shows a permission dialog on first run, to
     // show the dialog at a more appropriate time move this registration accordingly.
     // [START register_for_notifications]
-    if #available(iOS 10.0, *) {
-      // For iOS 10 display notification (sent via APNS)
-      UNUserNotificationCenter.current().delegate = self
 
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { _, _ in }
-      )
-    } else {
-      let settings: UIUserNotificationSettings =
-        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-      application.registerUserNotificationSettings(settings)
-    }
+    UNUserNotificationCenter.current().delegate = self
+
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: { _, _ in }
+    )
 
     application.registerForRemoteNotifications()
 

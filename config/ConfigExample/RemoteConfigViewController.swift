6946dedd1c9aeb8662ff7c9a6bbd6d0d6731e36f
Quan Pham <qdpham@google.com>
2023-01-19 15:11:41 -0800

    Add extra logging

--- config/ConfigExample/RemoteConfigViewController.swift
+++ config/ConfigExample/RemoteConfigViewController.swift
@@ -72,12 +72,18 @@ class RemoteConfigViewController: UIViewController {
     remoteConfig.configSettings = settings
       
     remoteConfig.add { RemoteConfigUpdate, Error in
+        NSLog("Received Realtime Signal")
         guard Error == nil else {
             DispatchQueue.main.async {
                 self.displayError(Error)
             }
             return
         }
+        
+        if (RemoteConfigUpdate?.updatedKeys.contains("realtime_rc_changed_params") != nil) {
+            NSLog("Received realtime_rc_changed_params")
+        }
+        
         self.remoteConfig.activate() { changed, error in
             guard error == nil else {
                 DispatchQueue.main.async {

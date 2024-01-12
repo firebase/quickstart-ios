c3174b2320d85b55a5b68a1436a6a864d9d35fc9
Paul Beusterien <paulbeusterien@google.com>
2024-01-11 17:22:42 -0800

    Fix typo

--- authentication/AuthenticationExample/Utility/Extensions.swift
+++ authentication/AuthenticationExample/Utility/Extensions.swift
@@ -69,7 +69,7 @@ extension UIViewController {
   public func displayError(_ error: Error?, from function: StaticString = #function) {
     guard let error = error else { return }
     print("ⓧ Error in \(function): \(error.localizedDescription)")
-    let message = "\(error.localizedDescription)\n\n Ocurred in \(function)"
+    let message = "\(error.localizedDescription)\n\n Occurred in \(function)"
     let errorAlertController = UIAlertController(
       title: "Error",
       message: message,

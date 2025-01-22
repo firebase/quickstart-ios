af5b0c87e0e896ca14f3c29d79ec5c6b69f41c51
Morgan Chen <morganchen12@gmail.com>
2025-01-22 14:38:17 -0800

    migrate to spm and remove fdl

--- inappmessaging/InAppMessagingExampleSwift/AppDelegate.swift
+++ inappmessaging/InAppMessagingExampleSwift/AppDelegate.swift
@@ -18,7 +18,6 @@ import UIKit
 
 import FirebaseCore
 import FirebaseInAppMessaging
-import FirebaseDynamicLinks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -30,39 +29,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Uncomment the following line to disable In-App Messaging auto-startup.
     // InAppMessaging.inAppMessaging().automaticDataCollectionEnabled = false
 
-    FirebaseOptions.defaultOptions()?.deepLinkURLScheme = "com.google.InAppMessagingExampleSwiftiOS"
     FirebaseApp.configure()
     return true
   }
 
-  func application(_ app: UIApplication,
-                   open url: URL,
-                   options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-    return application(app,
-                       open: url,
-                       sourceApplication: options[.sourceApplication] as? String,
-                       annotation: options[.annotation] as Any)
-  }
 
-  func application(_ application: UIApplication,
-                   open url: URL,
-                   sourceApplication: String?,
-                   annotation: Any) -> Bool {
-    let dynamicLink = DynamicLinks.dynamicLinks().dynamicLink(fromCustomSchemeURL: url)
-
-    if dynamicLink != nil {
-      if dynamicLink?.url != nil {
-        // Handle the deep link. For example, show the deep-linked content,
-        // apply a promotional offer to the user's account or show customized onboarding view.
-        // ...
-      } else {
-        // Dynamic link has empty deep link. This situation will happens if
-        // Firebase Dynamic Links iOS SDK tried to retrieve pending dynamic link,
-        // but pending link is not available for this device/App combination.
-        // At this point you may display default onboarding view.
-      }
-      return true
-    }
-    return false
-  }
 }

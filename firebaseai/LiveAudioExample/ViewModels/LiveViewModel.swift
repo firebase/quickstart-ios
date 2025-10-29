a61a45963ee89ae9dfef7f8a729f33acfb9ba7e3
Daymon <daymxn@google.com>
2025-10-29 12:28:44 -0500

    Move Color extension to new file

--- firebaseai/LiveAudioExample/ViewModels/LiveViewModel.swift
+++ firebaseai/LiveAudioExample/ViewModels/LiveViewModel.swift
@@ -264,32 +264,3 @@ class LiveViewModel: ObservableObject {
     )
   }
 }
-
-extension Color {
-  /// Creates a new `Color` instance from a hex string.
-  ///
-  /// Supports both RGB and RGBA hex strings.
-  init?(hex: String) {
-    let hex = hex.replacingOccurrences(of: "#", with: "").uppercased()
-
-    var rgb: UInt64 = 0
-    guard Scanner(string: hex).scanHexInt64(&rgb) else { return nil }
-
-    var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 1
-
-    if hex.count == 6 {
-      r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-      g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-      b = CGFloat(rgb & 0x0000FF) / 255.0
-    } else if hex.count == 8 {
-      r = CGFloat((rgb & 0xFF00_0000) >> 24) / 255.0
-      g = CGFloat((rgb & 0x00FF_0000) >> 16) / 255.0
-      b = CGFloat((rgb & 0x0000_FF00) >> 8) / 255.0
-      a = CGFloat(rgb & 0x0000_00FF) / 255.0
-    } else {
-      return nil
-    }
-
-    self.init(red: r, green: g, blue: b, opacity: a)
-  }
-}

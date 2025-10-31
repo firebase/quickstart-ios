d7c2e89efc4542f44af121652cf12d67477ac7ca
Paul Beusterien <paulbeusterien@google.com>
2025-10-31 13:57:20 -0700

    Remove template chat for now

--- firebaseai/FirebaseAIExample/ContentView.swift
+++ firebaseai/FirebaseAIExample/ContentView.swift
@@ -71,14 +71,6 @@ struct ContentView: View {
             Label("Chat", systemImage: "ellipsis.message.fill")
           }
           NavigationLink {
-            ConversationFromTemplateScreen(
-              firebaseService: firebaseService,
-              title: "Chat from Template"
-            )
-          } label: {
-            Label("Chat from Template", systemImage: "ellipsis.message")
-          }
-          NavigationLink {
             ConversationScreen(
               firebaseService: firebaseService,
               title: "Grounding",

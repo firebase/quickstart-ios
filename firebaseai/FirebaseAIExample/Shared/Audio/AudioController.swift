2d1a0ec4dde8b8c60035bcc2692499c00384a297
Nick Cooke <nickcooke@google.com>
2025-12-11 17:05:12 -0500

    style

--- firebaseai/FirebaseAIExample/Shared/Audio/AudioController.swift
+++ firebaseai/FirebaseAIExample/Shared/Audio/AudioController.swift
@@ -220,8 +220,8 @@ actor AudioController {
 
     private func handleRouteChange(notification: Notification) {
         guard let userInfo = notification.userInfo,
-            let reasonValue = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt,
-            let reason = AVAudioSession.RouteChangeReason(rawValue: reasonValue)
+              let reasonValue = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt,
+              let reason = AVAudioSession.RouteChangeReason(rawValue: reasonValue)
         else {
             return
         }
